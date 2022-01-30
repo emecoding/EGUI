a89949c7686b7d7d63b5eca9680582d3ef4532cb
emecoding <emecoding@gmail.com>
2022-01-30 10:23:47 +0200

    INITIAL

---
+++ build/CMakeFiles/EGUI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EGUI.dir/src/main.cpp.o"
+  "EGUI.pdb"
+  "EGUI"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EGUI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/eme/c++/EGUI
+# Build directory: /home/eme/c++/EGUI/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
